1a27bfef53ef75e4f8908099a0de6c80c0a6642b
Will <wmitbrodt@gmail.com>
2017-08-11 20:59:08 -0700

    created remove contact function

--- src/App.js
+++ src/App.js
@@ -1,33 +1,45 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
 
-const contacts = [
-  {
-    "id": "ryan",
-    "name": "Ryan Florence",
-    "email": "ryan@reacttraining.com",
-    "avatarURL": "http://localhost:5001/ryan.jpg"
-  },
-  {
-    "id": "michael",
-    "name": "Michael Jackson",
-    "email": "michael@reacttraining.com",
-    "avatarURL": "http://localhost:5001/michael.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "email": "tyler@reacttraining.com",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  }
-]
+
 
 
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        "id": "ryan",
+        "name": "Ryan Florence",
+        "email": "ryan@reacttraining.com",
+        "avatarURL": "http://localhost:5001/ryan.jpg"
+      },
+      {
+        "id": "michael",
+        "name": "Michael Jackson",
+        "email": "michael@reacttraining.com",
+        "avatarURL": "http://localhost:5001/michael.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "email": "tyler@reacttraining.com",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+    ]
+  }
+  // when removeContact is invoked, it's going to be passed the specific contact that was clicked on
+  removeContact = (contact) => {
+    this.setState((state) => ({
+      // then we're going to filter the current contacts on our state
+      // and we're going to remove where the state's c.id is not equal to the contact's id that was clicked on
+      contacts: state.contacts.filter((c) => c.id !== contact.id)
+      // will return a new contacts array, with the specific contact that was clicked on filtered out
+    }))
+  }
   render() {
     return (
        <div>
-         <ListContacts contacts={contacts} />
+         <ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts} />
        </div>
      )
   }
