85a7afb4bf91742664bc6b0bb543c15f89746a99
Will <wmitbrodt@gmail.com>
2017-08-12 02:33:47 -0700

    added a search for contacts

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import escapeRegExp from 'escape-string-regexp'
 import sortBy from 'sort-by'
 
-class ListContacts extends Component {
+class ListContacts extends Component { // 4. Because its state has changed, the ListContacts component re-renders.
   static propTypes = {
     contacts: PropTypes.array.isRequired,
     onDeleteContact: PropTypes.func.isRequired
@@ -18,23 +18,39 @@ class ListContacts extends Component {
     this.setState({ query: query.trim() })
   }
 
-  render () { // 4. Because its state has changed, the ListContacts component re-renders.
+  render () {
+    // object destructuring:
+    // basically, created 3 different variables: contacts, onDeleteContact, query
+    const { contacts, onDeleteContact } = this.props
+    const { query } = this.state
+
+    let showingContacts
+    if (query) {
+      // escape regular expressions and ignore uppercase
+      const match = new RegExp(escapeRegExp(query), 'i')
+      // filter by contact names
+      showingContacts = contacts.filter((contact) => match.test(contact.name))
+    } else {
+      showingContacts = contacts
+    }
+    // use the sortBy library to easily sort by name
+    showingContacts.sort(sortBy('name'))
+
     return (
       <div className='list-contacts'>
-        {JSON.stringify(this.state)}
         <div className='list-contacts-top'>
           <input
             className='search-contacts'
             type='text'
             placeholder='Search Contacts'
             // 1. The user enters text into the input field.
-            value={this.state.query}
+            value={query}
             // 2. An event listener invokes the updateQuery() function on every onChange event.
             onChange={(event) => this.updateQuery(event.target.value)}
           />
         </div>
         <ol className='contact-list'>
-          {this.props.contacts.map((contact) => (
+          {showingContacts.map((contact) => (
             <li key={contact.id} className='contact-list-item'>
               <div className='contact-avatar' style={{
                 backgroundImage: `url(${contact.avatarURL})`
@@ -43,7 +59,7 @@ class ListContacts extends Component {
                 <p>{contact.name}</p>
                 <p>{contact.email}</p>
               </div>
-              <button onClick={() => this.props.onDeleteContact(contact)} className='contact-remove'>
+              <button onClick={() => onDeleteContact(contact)} className='contact-remove'>
                 Remove
               </button>
             </li>
